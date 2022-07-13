45c74c6bf035743bdd3ebbe33caf9c2c082dc24f
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-07-13 11:38:37 +0900

    フロントプロジェクト作成

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { Top } from "./pages/Top";
+
+export const App: FC = () => {
+  return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Top></Top>}></Route>
+    //   </Routes>
+    // </BrowserRouter>
+    <div>
+      <div>aaaa</div>
+    </div>
+  );
+};
---
+++ frontend/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: `${__dirname}/dist`,
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
