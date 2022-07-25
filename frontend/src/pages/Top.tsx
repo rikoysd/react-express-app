200be4b87fd4e84ac6cf626d539093fb2441c41a
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-07-25 11:43:19 +0900

    トップページレイアウト修正

--- frontend/src/pages/Top.tsx
+++ frontend/src/pages/Top.tsx
@@ -1,5 +1,8 @@
 import { FC } from "react";
 import { useNavigate } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import top from "../img/top.png";
 
 export const Top: FC = () => {
   const navigate = useNavigate();
@@ -19,10 +22,67 @@ export const Top: FC = () => {
   };
 
   return (
-    <div>
-      <div>冷蔵庫管理アプリ</div>
-      <button onClick={onClickRegisterFood}>食材を登録する</button>
-      <button onClick={onClickRecordRecipes}>今日の献立を記録する</button>
-    </div>
+    <SContainer>
+      <STitleArea>
+        <div style={{ textAlign: "center" }}>冷蔵庫管理アプリ</div>
+        <STitle>Raku Limiter</STitle>
+        <div>
+          <img style={{ width: "370px" }} src={top} alt="girl" />
+        </div>
+      </STitleArea>
+      <div>
+        <div>
+          <Button
+            style={{
+              color: "black",
+              backgroundColor: "#FFAA2C",
+              fontFamily: "'Zen Maru Gothic', sans-serif",
+              boxShadow: "none",
+              width: "230px",
+              height: "45px",
+              borderRadius: "5px",
+              marginBottom: "40px",
+            }}
+            variant="contained"
+            onClick={onClickRegisterFood}
+          >
+            食材を登録する
+          </Button>
+        </div>
+        <div>
+          <Button
+            style={{
+              color: "black",
+              backgroundColor: "#FFAA2C",
+              fontFamily: "'Zen Maru Gothic', sans-serif",
+              boxShadow: "none",
+              width: "230px",
+              height: "45px",
+              borderRadius: "5px",
+            }}
+            variant="contained"
+            onClick={onClickRecordRecipes}
+          >
+            今日の献立を記録する{" "}
+          </Button>
+        </div>
+      </div>
+    </SContainer>
   );
 };
+
+const SContainer = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh",
+});
+
+const STitle = styled("div")({
+  fontSize: "35px",
+  textAlign: "center",
+});
+
+const STitleArea = styled("div")({
+  marginRight: "30px",
+});
