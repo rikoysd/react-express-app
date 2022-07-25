e7fb3a147a96c3fff478fe0bbbc4d5d424d979c3
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-07-25 14:16:37 +0900

    微修正

--- frontend/src/hooks/useFetchRefrigerator.ts
+++ frontend/src/hooks/useFetchRefrigerator.ts
@@ -1,16 +1,15 @@
 import axios from "axios";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import type { Refrigerator } from "../types/refrigerator";
-import { addHours } from "date-fns";
 
 export const useFetchRefrigerator = () => {
   const [foodList, setFoodList] = useState<Refrigerator[]>([]);
 
-  const getFoodList = () => {
+  const getFoodList = useCallback(() => {
     axios.get("http://localhost:3001/api/get/foodList").then((response) => {
       setFoodList(response.data);
     });
-  };
+  }, []);
 
   return { foodList, getFoodList };
 };
